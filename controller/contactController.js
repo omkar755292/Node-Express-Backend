2c7d47113e1d2c3bcd5b5f55bbce4e6e49b85dae
root <omkar13021@gmail.com>
2023-08-16 09:34:06 +0000

    created api for contact and added delete all api

--- controller/contactController.js
+++ controller/contactController.js
@@ -6,7 +6,7 @@ const contactModel = require('../models/contactModel');
 //@access: Public
 const getContacts = asyncHandler( async (req, res)=>{
     const getcontacts = await contactModel.find();
-    res.json(getcontacts);
+    res.json({getcontacts,message: "All messages available in Database"});
 });
 
 //@desc: Get Messages by id
@@ -16,9 +16,9 @@ const getContact = asyncHandler( async (req, res)=>{
     const getcontact = await contactModel.findById(req.params.id);
     if (!getcontact) {
         res.status(404);
-        throw new Error ("The message is not Found");
+        throw new Error ("The messages is not Found");
     }
-    res.json(getcontact);
+    res.json({getcontact, message:"This is message detail you requisted for"});
 });
 
 //@desc: Create New Message
@@ -31,8 +31,41 @@ const postContact = asyncHandler( async (req, res)=>{
         throw new Error("All Fields are Mandatory");
     }
     const createcontact = await contactModel.create(req.body);
-    res.json(createcontact);
-    console.log(req.body, "new message is created");
+    res.json({createcontact, message: "New message is created"});
 });
 
-module.exports = {getContact, getContacts, postContact}
+//@desc: Upadate existing Message
+//@methode: PUT /api/contact/
+//@access: Public
+const upadateContact = asyncHandler (async (req, res)=>{
+    const getcontact = await contactModel.findById(req.params.id);
+    if (!getcontact) {
+        res.status(404);
+        throw new Error ("The messages is not Found");
+    }
+    const updatecontact = await contactModel.findByIdAndUpdate(req.params.id, req.body,{new: true});
+    res.status(200).json({updatecontact, message: "This message is updated"});
+})
+
+//@desc: Delete existing Message
+//@methode: DELETE /api/contact/
+//@access: Public
+const deleteContact = asyncHandler (async (req, res)=>{
+    const getcontact = await contactModel.findById(req.params.id);
+    if (!getcontact) {
+        res.status(404);
+        throw new Error ("The messages is not Found");
+    }
+    const deletecontact = await contactModel.findByIdAndDelete(req.params.id);
+    res.status(200).json({deletecontact, message:"This message is deleted"});
+});
+
+//@desc: Get all Messages
+//@methode: GET /api/contact/
+//@access: Public
+const deleteContacts = asyncHandler( async (req, res)=>{
+    const deletecontacts = await contactModel.deleteMany();
+    res.json({deletecontacts, message: "All messages are deleted"});
+});
+
+module.exports = {getContact, getContacts, postContact, upadateContact, deleteContact, deleteContacts}
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,12 +1,13 @@
 const express = require ('express');
-const { getContacts, postContact, getContact } = require('../controller/contactController');
+const { getContacts, postContact, getContact, upadateContact, deleteContact, deleteContacts } = require('../controller/contactController');
 
 const contactRouter = express.Router();
 
 contactRouter.route('/').get(getContacts);
 contactRouter.route('/').post(postContact);
 contactRouter.route('/:id').get(getContact);
-contactRouter.route('/:id').put();
-contactRouter.route('/:id').delete();
+contactRouter.route('/:id').put(upadateContact);
+contactRouter.route('/:id').delete(deleteContact);
+contactRouter.route('/').delete(deleteContacts);
 
 module.exports = contactRouter;
